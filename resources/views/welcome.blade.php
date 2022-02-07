54a8d27a42e69275d644f0d61066ad49556852dd
nipulkm <nipulkm@gmail.com>
2022-02-07 21:35:13 +0600

    Design modified

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,10 +25,10 @@
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
-                        <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
+                        <a href="{{ url('/dashboard') }}" class="text-lg text-gray-700 dark:text-gray-500 underline">Dashboard</a>
                     @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Login as Owner</a>
-                        <a href="{{ route('login_as_employee') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Login as Employee</a>
+                        <a href="{{ route('login') }}" class="text-lg text-gray-700 dark:text-gray-500 underline">Login as Owner</a>
+                        <a href="{{ route('login_as_employee') }}" class="text-lg text-gray-700 dark:text-gray-500 underline">Login as Employee</a>
                     @endauth
                 </div>
             @endif
